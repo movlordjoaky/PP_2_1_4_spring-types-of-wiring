b634d8aaee0704be0bbfe621d1f2febac44458d8
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-07-19 12:41:21 +0300

    warnings

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -2,7 +2,6 @@ package koschei;
 
 import koschei.models.Duck5;
 import koschei.models.Ocean1;
-import koschei.models.Rabbit4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,7 +1,7 @@
 package koschei.models;
 
 public class Egg6 {
-    private Needle7 needle;
+    private final Needle7 needle;
 
     public Egg6(Needle7 needle) {
         this.needle = needle;
